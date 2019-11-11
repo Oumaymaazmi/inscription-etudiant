7efe4927de3698e71db9e097d0ba3fdffcd53f0a
momo <momo@DESKTOP-4DQG0OJ>
2019-11-11 17:44:56 +0100

    add same service sur departement2

--- src/main/java/com/fstg/inscription/rest/DepartementRest.java
+++ src/main/java/com/fstg/inscription/rest/DepartementRest.java
@@ -5,6 +5,16 @@
  */
 package com.fstg.inscription.rest;
 
+import com.fstg.inscription.bean.Departement;
+import com.fstg.inscription.bean.Filiere;
+import com.fstg.inscription.service.DepartementService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +24,34 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/inscription-api/departement")
-public class DepartementRest {
+public class DepartementRest  {
+   @Autowired
+    private DepartementService departementService;
+    @PostMapping("/")
+    public void save(@RequestBody Departement departement) {
+        departementService.save(departement);
+    }
+     @GetMapping("/")
+    public List<Departement> findAll() {
+        return departementService.findAll();
+    }
+   @GetMapping("/departement/{libelle}")
+    public Departement findByLibelle(@PathVariable String libelle) {
+        return departementService.findByLibelle(libelle);
+    }
+    @GetMapping("/departement/{filiere}")
+    public boolean findByFiliere(@RequestBody Departement departement,@PathVariable String libelle) {
+        return departementService.findByFiliere(departement, libelle);
+    }
+    @GetMapping("/departement/{departement}")
+    public List<Filiere> findAllFiliere(@PathVariable String libelle) {
+        return departementService.findAllFiliere(libelle);
+    }
+    @PutMapping("/{id}")
+    public Departement edit(long id, Departement departement) {
+        return departementService.edit(id, departement);
+    }
+   
+    
     
 }
